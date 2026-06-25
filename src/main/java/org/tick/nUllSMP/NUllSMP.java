0825b89bffc7a46cf4614e47555d1479669dff75
kilian159268 <kilian159268@gmail.com>
2026-06-25 21:35:32 +0200

    anfang

---
+++ src/main/java/org/tick/nUllSMP/NUllSMP.java
@@ -0,0 +1,17 @@
+package org.tick.nUllSMP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NUllSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
