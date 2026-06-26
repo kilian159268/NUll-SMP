6cd2bc455896232259371ba36c61aa665752dbfe
HBxxxxx <hannes-beck@gmx.net>
2026-06-26 23:25:38 +0200

    Files

--- src/main/java/org/tick/nUllSMP/NUllSMP.java
+++ src/main/java/org/tick/nUllSMP/NUllSMP.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NUllSMP extends JavaPlugin {
 
+    String consolePrefix = "[Null-SMP] ";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        //say Hello
+
+        System.out.println(consolePrefix + "Null-SMP Plugin has been enabled!");
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        System.out.println(consolePrefix + "Null-SMP Plugin has been disabled!");
+
     }
 }
